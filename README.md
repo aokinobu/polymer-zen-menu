ce67282d12bb44ae34d94e798aa8e412238f695a
aokinobu <aoki@bluetree.jp>
2019-05-01 05:03:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# polymer-zen-menu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/polymer-zen-menu)
---
+++ index.js
@@ -0,0 +1,27 @@
+import {PolymerElement, html} from '@polymer/polymer';
+
+class MyElement extends PolymerElement {
+
+  static get properties() { 
+    return {
+      xp: {
+        type: Number,
+        value: 0
+      }
+    }
+  }
+
+  static get template() {
+    return html`
+      <style> .xp { color: blue; } </style>
+      
+      <span class="xp" >Current XP: [[xp]]</span>
+    `;
+  }
+
+  ready() {
+    super.ready();
+  }
+}
+
+customElements.define('zen-menu', MyElement);
